6b1f1db4b9389d9edcdaf2267533a1bf69b34f40
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-10-19 16:37:14 -0500

    ✨ Updated layout to handle title and shell options

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,14 +2,19 @@ import Head from "next/head";
 import { childrenProps } from "../interfaces/children";
 import Shell from "./shell";
 
-const Layout = ({ children }: childrenProps) => {
+const Layout = ({ children, title, shell }: childrenProps) => {
   return (
     <>
       <Head>
-        <title>CougarCS - Admin Portal</title>
+        {title ? (
+          <title>{title}</title>
+        ) : (
+          <title>CougarCS - Admin Portal</title>
+        )}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Shell>{children}</Shell>
+      {shell ? <Shell>{children}</Shell> : <>{children}</>}
+      {/* <Shell>{children}</Shell> */}
     </>
   );
 };
