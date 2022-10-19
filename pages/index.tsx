c1b2fe139cb1d4633e80f9e06f39547c4e05985f
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-10-19 16:37:45 -0500

    ✨ Update homepage to provide shell option

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
+    <Layout shell>
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>
