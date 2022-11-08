71b8e79b731b68f1d6e8221eff6abc6f055b6b29
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-11-08 17:23:37 -0600

    🧐 Mock user data

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,13 @@
 import { Navbar } from "@mantine/core";
 
+const mockUser = {
+  contact_id: "e0765790-fb13-477f-92ed-ef658effb7d8",
+  uh_id: 1234567,
+  email: "bfilson0@bandcamp.com",
+  first_name: "Berry",
+  last_name: "Filson",
+};
+
 const Nav = () => {
   return (
     <>
