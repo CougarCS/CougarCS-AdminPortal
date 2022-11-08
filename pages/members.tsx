ec0a1281ba77c79ceee15cf032457522ccb1e9f8
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-11-08 17:07:47 -0600

    ✨ Created starter members page

---
+++ pages/members.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import Layout from "../components/layout";
+import { fetchMember } from "../utils/api";
+
+const Members: NextPage = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    (async () => {
+      const memberData = await fetchMember();
+      setData(memberData);
+
+      console.log(memberData);
+
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Layout shell>{loading ? <a>loading...</a> : <a>Data here</a>}</Layout>
+  );
+};
+
+export default Members;
