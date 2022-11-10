e3b0df326e04e2d16115622b5ac56d84b94b4bbb
Michelle Nguyen <michellehnguyen03@gmail.com>
2022-11-10 00:20:29 -0600

    deleted navbar test file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { MantineProvider, Button } from '@mantine/core';
 import Nav from "../components/nav";
-import { NavbarSimple } from '../components/navtest';
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>    
