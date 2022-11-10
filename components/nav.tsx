8ef87939708afa02cf053b74b13934169ae07d79
Michelle Nguyen <michellehnguyen03@gmail.com>
2022-11-10 00:20:01 -0600

    made rough draft of user and data display using Mantine's template

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,4 +1,5 @@
-import { Navbar } from "@mantine/core";
+import { Navbar, createStyles, Group, Code } from "@mantine/core";
+import { useState } from 'react';
 
 const mockUser = {
   contact_id: "e0765790-fb13-477f-92ed-ef658effb7d8",
@@ -7,10 +8,77 @@ const mockUser = {
   first_name: "Berry",
   last_name: "Filson",
 };
+const mockData = [
+  { link: "#", label: "Overview" },
+  { link: "#", label: "Contacts"},
+  { link: "#", label: "Memberships"},
+  { link: "#", label: "Ledger"},
+  { link: "#", label: "Events"}
+];
+
+const useStyles = createStyles((theme, _params, getRef) => {
+  return {
+    header: {
+      paddingBottom: theme.spacing.md,
+      marginBottom: theme.spacing.md * 1.5,
+      borderBottom: `1px solid ${
+        theme.colorScheme === 'dark'
+      }`,
+    },
+    footer: {
+      paddingTop: theme.spacing.md,
+      marginTop: theme.spacing.md,
+      borderTop: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      }`,
+    },
+    link: {
+      ...theme.fn.focusStyles(),
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.sm,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+      padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+      borderRadius: theme.radius.sm,
+      fontWeight: 500,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    }
+  },
+    
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor })
+          .background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    }
+  }
+});
+
 
 const Nav = () => {
+  const { classes, cx} = useStyles();
+  const [active, setActive] = useState('Overview');
+
+  const links = mockData.map((item) => (
+    <a
+    className = {cx(classes.link, { [classes.linkActive]: item.label === active })}
+    href={item.link}
+    key={item.label}
+    onClick={(event)=> {
+      event.preventDefault();
+      setActive(item.label);
+    }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
+
   return (
-    <>
       <Navbar
         className="NavbarItems"
         fixed={false}
@@ -22,13 +90,23 @@ const Nav = () => {
           base: 50,
         }}
       >
-        <Navbar.Section>{"Logo"}</Navbar.Section>
-        <Navbar.Section grow mt="md">
-          {"Overview"}
-          <div>
-            {"Contacts"}
-          </div>
+        <Navbar.Section grow>
+          <Group className={classes.header} position="apart">
+            <Code sx={{ fontWeight: 700}}> CougarCS</Code>
+          </Group>
+          {links}
         </Navbar.Section>
+        
+        <Navbar.Section className={classes.footer}>
+          <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+            <span>Change account</span>
+          </a>
+
+          <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+        
+            <span>Logout</span>
+          </a>
+      </Navbar.Section>
         <Navbar.Section>
             {mockUser.first_name} {mockUser.last_name}
           <div>
@@ -36,7 +114,7 @@ const Nav = () => {
           </div>
         </Navbar.Section>
       </Navbar>
-    </>
+ 
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { MantineProvider, Button } from '@mantine/core';
 import Nav from "../components/nav";
+import { NavbarSimple } from '../components/navtest';
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>    
       <div>
-        <Nav />
+        
+        <Nav/>
       </div>
     </MantineProvider>
   );
