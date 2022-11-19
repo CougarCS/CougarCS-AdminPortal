960e2bedcbc42bc4f7fc4a4b08f7c8d09e54dcb0
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2022-11-18 22:53:34 -0600

    MemberData type

--- types/types.ts
+++ types/types.ts
@@ -4,3 +4,15 @@ type loginData = {
 };
 
 export type { loginData };
+
+type MemberData = {
+  contact_id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  shirt_size_id: string;
+  uh_id: number;
+};
+
+export type { MemberData };
