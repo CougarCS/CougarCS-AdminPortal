d396318a7b47d1674688bd6da1dd727fc7027c39
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2022-11-20 12:47:20 -0600

    Add dead add and edit buttons to member page

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,3 +1,4 @@
+import { Group, Button } from "@mantine/core";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Layout from "../components/layout";
@@ -26,7 +27,22 @@ const Members: NextPage = () =>
   }, []);
 
   return (
-    <Layout shell>{loading ? <a>loading...</a> : <MembersTable data={data} />}</Layout>
+    <Layout shell>{loading ? <a>loading...</a> :
+      <div>
+        <h1>
+          Contacts
+        </h1>
+        <Group spacing={"xl"}>
+          <Button variant="outline" color="red" radius="xs" size="md">
+            Add
+          </Button>
+          <Button variant="outline" color="red" radius="xs" size="md">
+            Edit
+          </Button>
+        </Group>
+        <MembersTable data={data} />
+      </div>}
+    </Layout>
   );
 };
 
