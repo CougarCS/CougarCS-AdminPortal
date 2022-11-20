df4d32a655f44524053759dafe20e1c6426bac90
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2022-11-20 12:47:58 -0600

    Style refinements and bit of code cleanup for member table

--- components/membersTable/MembersTable.tsx
+++ components/membersTable/MembersTable.tsx
@@ -4,38 +4,47 @@ import { MemberData } from '../../types/types';
 
 // This uses the "Table With Sticky Header" starter from
 // https://ui.mantine.dev/category/tables
-const useStyles = createStyles((theme) => ({
-    header: {
-        position: 'sticky',
-        top: 0,
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-        transition: 'box-shadow 150ms ease',
-        '& th':
-        {
-            border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.red[6] : theme.colors.gray[2]
-                }`,
-        },
-        '&::after': {
-            content: '""',
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            bottom: 0,
-        },
-    },
 
-    bordering: {
-        '& td': {
-            border: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.red[6] : theme.colors.gray[2]
-                }`,
+const useStyles = createStyles((theme) =>
+{
+    const redQuery = theme.colorScheme === 'dark' ? theme.colors.red[5] : theme.white;
+    const borderWidth = "1px";
+    return {
+        // set top to -1 because text that was getting scrolled over was appearing
+        // in a slight gap at the top
+        header: {
+            position: 'sticky',
+            top: -1,
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+            transition: 'box-shadow 150ms ease',
+            '&::after': {
+                content: '""',
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                bottom: 0,
+            },
         },
-        borderCollapse: 'collapse',
-    },
 
-    scrolled: {
-        boxShadow: theme.shadows.lg,
-    },
-}));
+        borders: {
+            '& thead tr th': {
+                border: `${borderWidth} solid ${redQuery}`,
+                borderBottomWidth: 0,
+            },
+            '& tbody tr td': {
+                borderLeft: `${borderWidth} solid ${redQuery}`,
+                borderRight: `${borderWidth} solid ${redQuery}`,
+            },
+
+            borderCollapse: "collapse",
+            borderSpacing: 0,
+        },
+
+        scrolled: {
+            boxShadow: theme.shadows.lg,
+        },
+    };
+});
 
 interface MembersTableProps
 {
@@ -48,7 +57,7 @@ export function MembersTable({ data }: MembersTableProps)
     const [scrolled, setScrolled] = useState(false);
 
     const rows = data.map((row) => (
-        <tr className={cx(classes.bordering)} key={row.contact_id}>
+        <tr key={row.contact_id}>
             <td>{row.contact_id}</td>
             <td>{row.uh_id}</td>
             <td>{row.first_name}</td>
@@ -60,23 +69,25 @@ export function MembersTable({ data }: MembersTableProps)
         </tr>
     ));
 
+    const headers = (
+        <tr>
+            <th>Contact ID</th>
+            <th>UH ID</th>
+            <th>First</th>
+            <th>Last</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Shirt</th>
+            <th>Timestamp</th>
+        </tr>
+    );
+
     return (
-        <ScrollArea sx={{ height: "40%" }} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
-            <Table className={cx(classes.bordering)} sx={{ minWidth: 700 }}>
-                <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
-                    <tr>
-                        <th>Contact ID</th>
-                        <th>UH ID</th>
-                        <th>First</th>
-                        <th>Last</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Shirt</th>
-                        <th>Timestamp</th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </Table>
-        </ScrollArea>
+        <Table  className={cx(classes.borders)} sx={{ marginTop: "1rem", minWidth: 700 }}>
+            <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+                {headers}
+            </thead>
+            <tbody>{rows}</tbody>
+        </Table>
     );
 }
