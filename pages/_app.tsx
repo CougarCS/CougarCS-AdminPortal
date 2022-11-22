fb7b82ee270701f5d66b5366987f273cf0cb409a
Michelle Nguyen <michellehnguyen03@gmail.com>
2022-11-22 13:05:36 -0600

    return _app.tsx back to normal state and temporarily moved navbar under 'pages' for testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import { MantineProvider, Button } from '@mantine/core';
-import Nav from "../components/nav";
-export default function App() {
+import { MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>    
-      <div>
-        <Nav/>
-      </div>
+    <Component {...pageProps} />   
     </MantineProvider>
   );
 }
