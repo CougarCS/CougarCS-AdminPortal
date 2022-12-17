9c381baeb6869fa06d9e69c163a2da0ca6ddf65d
John <44646589+Junnyyy@users.noreply.github.com>
2022-12-17 01:48:55 -0600

    🚨 Run Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,31 +6,31 @@ const Home: NextPage = () => {
   return (
     <Layout shell>
       <div className="pr-8">
-        <main className="min-h-screen flex flex-col justify-center items-center pl-16">
+        <main className="flex min-h-screen flex-col items-center justify-center pl-16">
           <h1 className="m-0 text-7xl">
             Welcome to{" "}
             <a
-              className="hover:underline text-blue-400 font-bold"
+              className="font-bold text-blue-400 hover:underline"
               href="https://nextjs.org"
             >
               Next.js!
             </a>
           </h1>
 
-          <p className="mt-16 leading-6 text-base">
+          <p className="mt-16 text-base leading-6">
             Get started by editing{" "}
-            <code className="bg-zinc-800 rounded-md p-3 text-base font-mono">
+            <code className="rounded-md bg-zinc-800 p-3 font-mono text-base">
               pages/index.tsx
             </code>
           </p>
         </main>
 
-        <footer className="flex pt-8 justify-center items-center">
+        <footer className="flex items-center justify-center pt-8">
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex justify-center items-center"
+            className="flex items-center justify-center"
           >
             Powered by{" "}
             <span className="ml-2 h-4 invert">
