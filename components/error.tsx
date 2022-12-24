1888f274dcbdae9179f9c320e341e10f0f7032c2
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-12-24 01:14:51 -0600

    ✨ Create error component

---
+++ components/error.tsx
@@ -0,0 +1,19 @@
+import { Alert } from "@mantine/core";
+import { childrenProps } from "../types/types";
+import { BiErrorCircle } from "react-icons/bi";
+
+const Error = ({ children }: childrenProps) => {
+  return (
+    <>
+      <Alert
+        className="absolute bottom-6 right-6 w-1/5 min-w-max bg-red-500 py-3 px-5 text-black"
+        title="Error"
+        icon={<BiErrorCircle />}
+      >
+        <a>{children}</a>
+      </Alert>
+    </>
+  );
+};
+
+export default Error;
