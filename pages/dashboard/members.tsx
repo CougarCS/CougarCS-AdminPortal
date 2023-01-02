2b69d271cef9c462d7cc017f6694fa5ad3e6d74a
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-01-01 23:47:35 -0600

    ⚗️ Data post example

--- pages/dashboard/members.tsx
+++ pages/dashboard/members.tsx
@@ -1,14 +1,21 @@
 import type { NextPage } from "next";
 import Layout from "../../components/layout";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import fetcher from "../../utils/fetcher";
 import Error from "../../components/error";
 import { useState } from "react";
-import { Loader } from "@mantine/core";
+import { Button, Loader } from "@mantine/core";
+import poster from "../../utils/poster";
+import Success from "../../components/success";
 
 const Members: NextPage = () => {
   const { data, error, isLoading } = useSWR("/api/members", fetcher);
-  const [errorMessage] = useState("Unable to retrieve member data.");
+  const [isError, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(
+    "Unable to retrieve member data."
+  );
+  const [isSuccess, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
   if (error) {
     return (
@@ -28,9 +35,42 @@ const Members: NextPage = () => {
     );
   }
 
+  const user = {
+    uh_id: 1234567,
+    email: "eklug0@ibm.com",
+    first_name: "Esmaria",
+    last_name: "Klug",
+    phone_number: 3271982939,
+    shirt_size_id: "L",
+  };
+
   return (
     <Layout shell>
       {isLoading ? <a>loading...</a> : <a>{data[0].contact_id}</a>}
+      <br />
+      <Button
+        className="rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700 "
+        onClick={async () => {
+          setError(false);
+          setSuccess(false);
+
+          const res = await poster("/api/members", user);
+
+          if (res.error) {
+            setError(true);
+            setErrorMessage(res.description);
+            return;
+          }
+
+          setSuccess(true);
+          setSuccessMessage(`${user.first_name} was successfully added.`);
+          mutate("/api/members", data, false);
+        }}
+      >
+        Add Member
+      </Button>
+      {isError ? <Error>{errorMessage}</Error> : <></>}
+      {isSuccess ? <Success>{successMessage}</Success> : <></>}
     </Layout>
   );
 };
