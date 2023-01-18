2a58dfd80c4ba4f67486101ae85e1e7e3b67bf4b
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-01-18 00:06:20 -0600

    🔥 Removed fragments

--- components/error.tsx
+++ components/error.tsx
@@ -4,15 +4,13 @@ import { BiErrorCircle } from "react-icons/bi";
 
 const Error = ({ children }: childrenProps) => {
   return (
-    <>
-      <Alert
-        className="absolute bottom-6 right-6 w-1/5 min-w-max bg-red-600 py-3 px-5 text-black"
-        title="Error"
-        icon={<BiErrorCircle />}
-      >
-        <a>{children}</a>
-      </Alert>
-    </>
+    <Alert
+      className="absolute bottom-6 right-6 w-1/5 min-w-max bg-red-600 py-3 px-5 text-black"
+      title="Error"
+      icon={<BiErrorCircle />}
+    >
+      <a>{children}</a>
+    </Alert>
   );
 };
 
--- components/success.tsx
+++ components/success.tsx
@@ -4,15 +4,13 @@ import { BiCheckCircle } from "react-icons/bi";
 
 const Success = ({ children }: childrenProps) => {
   return (
-    <>
-      <Alert
-        className="absolute bottom-6 right-6 w-1/5 min-w-max bg-green-500 py-3 px-5 text-black"
-        title="Success"
-        icon={<BiCheckCircle />}
-      >
-        <a>{children}</a>
-      </Alert>
-    </>
+    <Alert
+      className="absolute bottom-6 right-6 w-1/5 min-w-max bg-green-500 py-3 px-5 text-black"
+      title="Success"
+      icon={<BiCheckCircle />}
+    >
+      <a>{children}</a>
+    </Alert>
   );
 };
 
