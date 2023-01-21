b0301cc246e4aad9db8c288f8fdfa80749f2b784
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-01-21 15:42:20 -0600

    added keys to maps and some padding to columns

--- components/dataTable/DataTable.tsx
+++ components/dataTable/DataTable.tsx
@@ -9,15 +9,15 @@ interface DataTableProps
 export const DataTable = ({ columns, rows, className }: DataTableProps) =>
 {
     const styledHeaderColumns = columns?.map(columnTitle =>
-        <th className="border border-red-400 bg-black px-2 py-2">
+        <th key={columnTitle} className="border border-red-400 bg-black px-2 py-2">
             {columnTitle}
         </th>);
 
     // Outermost map is for each row, innermost map is for each column in that row
-    const styledRows = rows?.map((rowData: any[]) =>
-        <tr className="cursor-pointer border border-y-zinc-700 text-gray-300 first:border-t-0 last:border-b-red-400 hover:bg-zinc-800">
-            {rowData.map((rowCol: any) =>
-                <td className="border-x border-x-red-400 px-1 py-1.5">
+    const styledRows = rows?.map((rowData: any[], index) =>
+        <tr key={index} className="cursor-pointer border border-y-zinc-700 text-gray-300 first:border-t-0 last:border-b-red-400 hover:bg-zinc-800">
+            {rowData.map((rowCol: any, index) =>
+                <td key={index} className="border-x border-x-red-400 px-3 py-1.5">
                     {rowCol}
                 </td>)}
         </tr>);
