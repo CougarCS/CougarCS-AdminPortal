ec7f470013812cc08d1f14e7d7272c8c39011fbb
Brandon Chung <chung.brandon123@gmail.com>
2023-04-11 23:28:43 -0500

    Implement edit functionality for modal

--- components/membersModal/viewMemberModal.tsx
+++ components/membersModal/viewMemberModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { MemberInformationModal } from "./memberInformationModal";
+import { UpdateMemberModal } from "./updateMemberModal";
 import { memberType } from "../../types/types";
 
 type viewMemberModalProps = {
@@ -14,7 +15,7 @@ export const ViewMemberModal = ({
   setModalOpen,
   member,
 }: viewMemberModalProps) => {
-  const [editingMember, setEditingMember] = useState<boolean>(false);
+  const [isUpdatingMember, setIsUpdatingMember] = useState<boolean>(false);
 
   return (
     <Modal
@@ -25,17 +26,27 @@ export const ViewMemberModal = ({
       className="flex h-screen w-full flex-col items-center justify-center bg-black bg-opacity-80"
       ariaHideApp={false}
     >
-      <div className="h-fit w-fit min-w-[30rem] max-w-xl rounded-md bg-sidebarBG p-7 text-white">
+      <div className="h-fit w-[30rem] rounded-md bg-sidebarBG p-7 text-white">
         <h1 className="text-4xl">
           {member.first_name} {member.last_name}
         </h1>
-        <h2 className="mb-2 text-xl">ID: {member.uh_id}</h2>
+        <h2 className="mb-2 border-b-[1px] border-gray-600 pb-2 text-xl">
+          ID: {member.uh_id}
+        </h2>
 
-        <MemberInformationModal
-          setModalOpen={setModalOpen}
-          member={member}
-          setEditingMember={setEditingMember}
-        />
+        {isUpdatingMember ? (
+          <UpdateMemberModal
+            setModalOpen={setModalOpen}
+            member={member}
+            setIsUpdatingMember={setIsUpdatingMember}
+          />
+        ) : (
+          <MemberInformationModal
+            setModalOpen={setModalOpen}
+            member={member}
+            setIsUpdatingMember={setIsUpdatingMember}
+          />
+        )}
       </div>
     </Modal>
   );
