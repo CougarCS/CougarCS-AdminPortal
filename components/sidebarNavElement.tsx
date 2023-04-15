307ee14bbbf7263784caaff38774da0b98248b79
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-04-14 22:32:12 -0500

    #45 tweaked style to match figma

--- components/sidebarNavElement.tsx
+++ components/sidebarNavElement.tsx
@@ -10,10 +10,12 @@ export const NavElement = ({ children, onClick, active }: NavElementProps) =>
 {
   return (
     <button
-      className={`flex items-center w-full text-gray-600 hover:bg-gray-50 hover:text-gray-900 p-3 ${active ? 'bg-gray-100 text-gray-900' : ''}`}
+      className={`h-12 w-full text-lg ${active ? 'text-white border-l-4 border-l-white' : 'text-gray-400'}`}
       onClick={() => onClick()}
     >
-      {children}
-    </button >
+      <div className="mx-auto h-9 w-4/5 rounded-md flex items-center justify-center gap-3 hover:bg-neutral-700 hover:text-white">
+        {children}
+      </div>
+    </button>
   );
 };
