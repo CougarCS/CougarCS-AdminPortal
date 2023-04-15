a93d2123bf84d0b84593247af9b3646d22891aae
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-04-14 22:36:50 -0500

    #45 corrected branding

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaHome, FaUsers, FaCalendarAlt, FaCog, FaSignOutAlt } from 'react-icons/fa';
 import { NavElement } from './sidebarNavElement';
 import { useRouter } from 'next/router';
-
+import Image from 'next/image';
 
 // moving logout function here for the sake of keeping layout file cleaner,
 // and because it's not really reused so it can sit here anyway
@@ -17,13 +17,16 @@ const Sidebar = () =>
 
   return (
     <div id="sidebar" className="min-h-full w-56 bg-sidebarBG flex justify-between flex-col">
-      <div className="flex items-center justify-center h-19 bg-sidebarBG px-4">
-        <img src="images/CougarCS-logo.png" alt="Logo" className="h-10" />
+      <div className="flex items-center justify-center  bg-sidebarBG p-8">
+        <Image
+          src="/images/CougarCS-logo.png"
+          alt="CougarCS Logo"
+          width={64}
+          height={64}
+        />
         <div>
-          <span className="text-lg font-bold text-white">CougarCS</span>
-        </div>
-        <div>
-          <span className="text-sm font-light text-white">Admin Portal</span>
+          <p className="text-lg font-bold text-white">CougarCS</p>
+          <p className="text-sm font-light text-white">Admin Portal</p>
         </div>
 
       </div>
@@ -37,9 +40,6 @@ const Sidebar = () =>
             </NavElement>
           );
         })}
-
-
-
       </div>
       <div className="flex flex-col justify-end mt-auto">
         <button onClick={() => { }} className="bg-red-500 text-white px-4 py-2 text-sm uppercase mt-3 mx-3 rounded-full hover:bg-red-600">Logout</button>
