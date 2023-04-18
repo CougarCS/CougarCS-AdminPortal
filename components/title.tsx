8415012c8bccf844bcd8818276ab83a25364cb4b
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-04-18 13:50:08 -0500

    title style tweaks

--- components/title.tsx
+++ components/title.tsx
@@ -1,16 +1,11 @@
 import { titleProps } from "../types/types";
 
-
 export const Title = ({ title, subtitle, children }: titleProps) => {
     return (
-        <div className="border-b-2 border-gray-800 ">
+        <div className="border-b-2 border-neutral-800 pb-4">
             <h1 className="text-5xl font-semibold text-red-600">{title}</h1>
-            <h2 className="text-x1 font-normal text-white pt-1 pb-2">{subtitle}</h2>
+            <h2 className="text-xl font-normal text-white mt-1">{subtitle}</h2>
             {children}
         </div>
-
     );
-
 };
-
-
