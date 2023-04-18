b6021f7ece42d2d361e1b93fdca37981e093a406
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-04-18 16:12:44 -0500

    Added member sort/filter controls to contacts

--- pages/dashboard/members.tsx
+++ pages/dashboard/members.tsx
@@ -10,13 +10,18 @@ import { toast } from "sonner";
 import { DataTable } from "../../components/dataTable/DataTable";
 import { Title } from "../../components/title";
 import { ViewMemberModal } from "../../components/membersModal/viewMemberModal";
+import { SelectInput } from "../../components/selectInput";
+import SearchBox from "../../components/searchBox";
+import { TextInput } from "../../components/textInput";
 
-const Members: NextPage = () => {
+const Members: NextPage = () =>
+{
   const { data, error, isLoading } = useSWR("/api/members", fetcher);
   const [isError, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState(
     "Unable to retrieve member data."
   );
+
 
   const [modalOpen, setModalOpen] = useState(false);
   const [modalData, setModalData] = useState<memberType>({
@@ -30,7 +35,15 @@ const Members: NextPage = () => {
     timestamp: "01/01/1970",
   });
 
-  const members = data as memberType[];
+  type paginationOptions = "20" | "50" | "100" | "All";
+  const [paginationCount, setPaginationCount] = useState<paginationOptions>("20");
+
+  const sortOpts = ["First", "Last", "UH ID", "Email", "Timestamp"] as const;
+  type sorts = typeof sortOpts[number];
+  const [sortBy, setSortBy] = useState<sorts>("First");
+
+  // const presentableData : memberType[][] = searchSortPaginate();
+
   const schema = {
     "UH ID": "uh_id",
     First: "first_name",
@@ -41,7 +54,8 @@ const Members: NextPage = () => {
     Timestamp: "timestamp",
   };
 
-  if (error) {
+  if (error)
+  {
     toast.error(`Contacts Error: ${errorMessage}`);
     return (
       <Layout>
@@ -57,7 +71,8 @@ const Members: NextPage = () => {
     );
   }
 
-  if (isLoading) {
+  if (isLoading)
+  {
     return (
       <Layout>
         <div className="grid h-screen place-content-center">
@@ -73,6 +88,41 @@ const Members: NextPage = () => {
       <Title
         title="Contacts"
         subtitle="All past, present, and future? CougarCS members and event attendees.">
+        <div className="flex flex-row">
+          <div className="mt-2 flex flex-col gap-2">
+            <div>
+              <span className="text-lg">Contacts per page: </span>
+              <SelectInput name="pagination" ariaLabel="Contacts Per Page" height="h-fit"
+                width="w-fit"
+                textSize="text-lg"
+                options={["20", "50", "100", "All"]}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                {
+                  const { value, name } = e.target;
+                  setPaginationCount(value as paginationOptions);
+                }}
+                value={paginationCount}
+              />
+            </div>
+
+            <div>
+              <span className="text-lg">Sort members by: </span>
+              <SelectInput name="sortBy" ariaLabel="Sort Data By" height="h-fit"
+                width="w-fit"
+                textSize="text-lg"
+                options={["First", "Last", "UH ID", "Email", "Timestamp"]}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                {
+                  const { value, name } = e.target;
+                  setSortBy(value as sorts);
+                }}
+                value={sortBy}
+              />
+            </div>
+          </div>
+          <TextInput label="" name="memberSearch" placeholder="Search Members" className="ml-auto mt-auto w-2/5" />
+
+        </div>
       </Title>
 
       <ViewMemberModal
@@ -82,39 +132,45 @@ const Members: NextPage = () => {
       />
 
       <br />
-      <button
-        onClick={async () => {
-          setError(false);
-
-          const res = await poster("/api/members", modalData);
-
-          if (res.error) {
-            setError(true);
-            setErrorMessage(res.description);
-            toast.error(`Contacts Error: ${res.description}`);
-            return;
-          }
-
-          toast.success(`Successfully added ${modalData.first_name}.`);
-          mutate("/api/members", data, false);
-        }}
-        className="h-9 rounded-sm bg-red-600 px-4 text-sm font-semibold text-white hover:bg-red-700"
-      >
-        Add Member
-      </button>
 
       {isLoading ? (
         <a>loading...</a>
       ) : (
-        <DataTable
-          className="mt-4"
-          schema={schema}
-          data={data}
-          rowClick={(modalData) => {
-            setModalData(modalData);
-            setModalOpen(true);
-          }}
-        />
+        <>
+          <DataTable
+            className="mt-4"
+            schema={schema}
+            data={data}
+            rowClick={(modalData) =>
+            {
+              setModalData(modalData);
+              setModalOpen(true);
+            }}
+          />
+          <button
+            onClick={async () =>
+            {
+              setError(false);
+
+              const res = await poster("/api/members", modalData);
+
+              if (res.error)
+              {
+                setError(true);
+                setErrorMessage(res.description);
+                toast.error(`Contacts Error: ${res.description}`);
+                return;
+              }
+
+              toast.success(`Successfully added ${modalData.first_name}.`);
+              mutate("/api/members", data, false);
+            }}
+            className="h-9 rounded-sm bg-red-600 px-4 text-sm font-semibold text-white hover:bg-red-700"
+          >
+            Add Member
+          </button>
+        </>
+
       )}
     </Layout>
   );
