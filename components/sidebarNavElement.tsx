6013ee059869b7d75c4b21b441303022e6aa285e
See Jay <39662993+ChrisOh431@users.noreply.github.com>
2023-04-23 04:18:40 -0500

    added events page to sidebar + fixed some styles

--- components/sidebarNavElement.tsx
+++ components/sidebarNavElement.tsx
@@ -10,10 +10,10 @@ export const NavElement = ({ children, onClick, active }: NavElementProps) =>
 {
   return (
     <button
-      className={`mb-3 h-12 w-full text-lg ${active ? 'text-white border-l-4 border-l-white' : 'text-gray-400'}`}
+      className={`mb-4 h-12 w-full text-lg ${active ? 'text-white border-l-4 border-l-white' : 'text-gray-400 border-l-4 border-l-transparent'}`}
       onClick={() => onClick()}
     >
-      <div className="mx-auto h-9 w-4/5 rounded-md flex items-center justify-center gap-3 hover:bg-neutral-700 hover:text-white">
+      <div className="mx-auto h-9 w-4/5 pl-4 rounded-md flex items-center justify-start gap-3 hover:bg-neutral-700 hover:text-white">
         {children}
       </div>
     </button>
