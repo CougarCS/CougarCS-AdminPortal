d0c2dfdb6fe7ba80beb5adf8531d60ab4636444b
Alan Johnson <alanjcd2004@gmail.com>
2023-04-25 02:07:22 -0500

    completed searchSortPaginate()

--- utils/searchSortPaginate.ts
+++ utils/searchSortPaginate.ts
@@ -12,14 +12,48 @@ import { SSPConfig, memberType } from "../types/types";
 
 // this is just returning data for now since the func isn't written
 
-function createCompareFn<T extends Object>(
-  property: keyof T,
-  sort_order: "ascending" | "descending"
+
+export function searchSortPaginate(data: memberType[], cfg: SSPConfig): memberType[][] {
+
+  // Search
+  let newArr: memberType[] = [];
+  let search: string = "";
+
+  if (cfg.query != undefined) {
+    data.forEach((val) => {
+      search = "";
+      search = (JSON.stringify(val)).toLowerCase();
+      let q: any = cfg.query?.toLocaleLowerCase();
+      if (search.indexOf(q) != -1)
+        newArr.push(val);
+    });
+  }
+  else {
+    newArr = data;
+  }
+
+  // Sort
+  newArr?.sort(comparison(cfg.sort.property, cfg.sort.dir));
+
+  // Paginate
+  const paginate = newArr?.slice(0, cfg.paginate);
+
+
+
+  const output: memberType[][] = [];
+  output.push(paginate);
+
+  return output;
+};
+
+function comparison(
+  property: string | number,
+  direction: "ascending" | "descending"
 ) {
-  const compareFn = (a: T, b: T) => {
+  const compareFn = (a: any, b: any) => {
     const val1 = a[property];
     const val2 = b[property];
-    const order = (sort_order !== "descending") ? 1 : -1;
+    const order = (direction == "ascending") ? 1 : -1;
 
     switch (typeof val1) {
       case "number": {
@@ -38,66 +72,8 @@ function createCompareFn<T extends Object>(
     }
   };
   return compareFn;
-}
+};
 
-export function searchSortPaginate(data: memberType[], cfg: SSPConfig): memberType[][] {
-
-
-  let newArr: memberType[] = [];
-  let search: string = "";
-
-  if (cfg.query != undefined) {
-    data.forEach((val) => {
-      search = "";
-      search = JSON.stringify(val);
-      let q: any = cfg.query;
-      if (search.indexOf(q) != -1)
-        newArr.push(val);
-
-    });
-  }
-  else {
-    newArr = data;
-  }
-
-  newArr.sort(createCompareFn(cfg.sort.property, cfg.sort.dir));
-
-  // const order = (cfg.sort.dir == "ascending") ? 1 : -1;
-
-  // switch (cfg.sort.property) {
-  //   case "uh_id":
-  //     newArr.sort((a, b) => (a.uh_id > b.uh_id) ? order : -1 * order);
-  //     break;
-  //   case "first_name":
-  //     newArr.sort((a, b) => (a.first_name > b.first_name) ? order : -1 * order);
-  //     break;
-  //   case "last_name":
-  //     newArr.sort((a, b) => (a.last_name > b.last_name) ? order : -1 * order);
-  //     break;
-  //   case "email":
-  //     newArr.sort((a, b) => (a.email > b.email) ? order : -1 * order);
-  //     break;
-  //   case "phone_number":
-  //     newArr.sort((a, b) => (a.phone_number > b.phone_number) ? order : -1 * order);
-  //     break;
-  //   case "shirt_size_id":
-  //     newArr.sort((a, b) => (a.shirt_size_id > b.shirt_size_id) ? order : -1 * order);
-  //     break;
-  //   case "timestamp":
-  //     newArr.sort((a, b) => (a.timestamp > b.timestamp) ? order : -1 * order);
-  //     break;
-
-  // }
-
-  const paginate = newArr.slice(0, cfg.paginate);
-
-
-  const output: memberType[][] = [];
-
-  output.push(paginate);
-
-  return output;
-}
 
 /*
 searchSortPaginate([], {
